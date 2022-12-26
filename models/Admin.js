474f15fb1ac176acb73ea53f08f2cd0e57f99ba3
Srinitish Srinivasan <srinitishsri@gmail.com>
2022-12-26 12:24:33 +0530

    admin db

---
+++ models/Admin.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose");
+const passportLocalMongoose=require("passport-local-mongoose");
+
+const adminSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+
+})
+
+adminSchema.plugin(passportLocalMongoose);
+const Admin=mongoose.model("Admin",adminSchema);
+module.exports=Admin;
